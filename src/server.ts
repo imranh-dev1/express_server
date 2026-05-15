2452a675329f70f57e29fdab4e3810b052982ce4
imran hossain <imranme.global@gmail.com>
2026-05-15 19:00:26 +0600

    Postgres Databases connnected

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,33 @@
 import express, { type Application, type Request, type Response } from 'express'
+import { Pool } from 'pg'
 
 const app: Application = express()
 const port = 5000
 
 app.use(express.json())
+const pool = new Pool({
+    connectionString: "postgresql://neondb_owner:npg_Lz0vQ9VIORZh@ep-proud-surf-aqdmiayp-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
+
+const initDB = async () => {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(20),
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            age INT,
+            country VARCHAR(255),
+            is_active BOOLEAN DEFAULT true,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+        );
+    `);
+
+    console.log("Database connected successfully!");
+}
+
+initDB();
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({
